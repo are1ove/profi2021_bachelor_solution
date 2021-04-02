4306e0f91c7cb4e05edb7302f565344f09a16f2c
are1ove <stakanov.valerij@mail.ru>
2021-04-02 15:19:42 +0300

    Add zoom func

--- scripts/simple_move.py
+++ scripts/simple_move.py
@@ -101,6 +101,21 @@ class SimpleMover():
             self.cmd_vel_pub.publish(twist_msg)
             self.rate.sleep()
 
+    def zoom(self, cv_image, scale):
+        height, width, _ = cv_image.shape
+        # print(width, 'x', height)
+        # prepare the crop
+        centerX, centerY = int(height / 2), int(width / 2)
+        radiusX, radiusY = int(scale * height / 100), int(scale * width / 100)
+
+        minX, maxX = centerX - radiusX, centerX + radiusX
+        minY, maxY = centerY - radiusY, centerY + radiusY
+
+        cv_image = cv_image[minX:maxX, minY:maxY]
+        cv_image = cv2.resize(cv_image, (width, height))
+
+        return cv_image
+
     def line_detect(self, msg):
         # Create a mask
         # cv_image_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
