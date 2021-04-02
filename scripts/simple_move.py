085c453c27909e5b5a79388c7ce8eb233fbd9d13
are1ove <stakanov.valerij@mail.ru>
2021-04-02 15:29:00 +0300

    Add black edges

--- scripts/simple_move.py
+++ scripts/simple_move.py
@@ -122,7 +122,7 @@ class SimpleMover():
         cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
         cv_image = self.zoom(cv_image, scale=20)
         cv_image = cv2.add(cv_image, np.array([-50.0]))
-        mask = cv2.inRange(cv_image, (130, 130, 130), (255, 255, 255))
+        mask = cv2.inRange(cv_image, (0, 0, 0), (60, 60, 60))
         kernel = np.ones((3, 3), np.uint8)
         mask = cv2.erode(mask, kernel, iterations=5)
         mask = cv2.dilate(mask, kernel, iterations=9)
