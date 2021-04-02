130c6505f9d84ab08439c32084c4c20a18ed37a5
are1ove <stakanov.valerij@mail.ru>
2021-04-02 19:12:40 +0300

    desired hight

--- scripts/simple_move.py
+++ scripts/simple_move.py
@@ -120,7 +120,7 @@ class SimpleMover():
         # Create a mask
         # cv_image_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
-        #cv_image = self.zoom(cv_image, scale=int(self.altitude_desired) * 20)
+        cv_image = self.zoom(cv_image, scale=int(self.altitude_desired) * 20)
         # cv_image = cv2.add(cv_image, np.array([-50.0]))
         mask = cv2.inRange(cv_image, (0, 0, 0), (20, 20, 20))
         kernel = np.ones((3, 3), np.uint8)
